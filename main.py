6cb459274ae24d580b965b03f6cabcd5578bae49
dunyasyedhassan <147941390+dunyasyedhassan@users.noreply.github.com>
2024-03-06 15:08:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("front.html")
+    return render_template("welcome.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
