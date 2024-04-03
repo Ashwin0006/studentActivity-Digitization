a55dfc55d6e55e20d400d051a1243efba7b3ba10
Dunya Syed Hassan <dunya@Dunyas-MacBook-Air.local>
2024-04-03 14:01:26 +0530

    welcome redirect fixed

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return render_template("welcome.html")
 
+@app.route("/front", methods=['POST', 'GET'])
+def front():
+    return render_template("front.html")
+
+@app.route("/student", methods=['POST', 'GET'])
+def student():
+    return render_template("student.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
